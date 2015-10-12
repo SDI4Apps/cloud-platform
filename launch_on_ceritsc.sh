dd1568c2881aa415f8d5afeb2c3438c1a1a06aad
Martin Kuba <makub@ics.muni.cz>
2015-10-12 11:55:28 +0200

    add files

---
+++ launch_on_ceritsc.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USER_DATA_FILE=/home/makub/sdi4apps/kontext/test_user_data.yaml
+CONTEXT_FILE=/tmp/ctx.$$
+TEMPLATE_ID=2941
+
+echo "starting virtual image OpenNebula using custom user-data"
+
+if [ ! -f "$USER_DATA_FILE" ] ; then
+  echo "file $USER_DATA_FILE not found"
+  exit 1
+fi
+
+cat >$CONTEXT_FILE <<"EOF"
+CONTEXT=[
+  EMAIL="$USER[EMAIL]",
+  PUBLIC_IP="$NIC[IP]",
+  SSH_KEY="$USER[SSH_KEY]",
+  TARGET="vdb",
+  TOKEN="YES",
+  VM_GID="$GID",
+  VM_GNAME="$GNAME",
+  VM_ID="$VMID",
+  VM_UID="$UID",
+  VM_UNAME="$UNAME",
+  USERDATA_ENCODING="base64",
+EOF
+echo >>$CONTEXT_FILE -n '  USER_DATA="'
+base64 >>$CONTEXT_FILE -w 0 $USER_DATA_FILE
+cat >>$CONTEXT_FILE <<EOF
+"
+]
+EOF
+onetemplate instantiate -v $TEMPLATE_ID $CONTEXT_FILE
