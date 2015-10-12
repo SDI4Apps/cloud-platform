4763b5159fe9351bdd37b2f5d3fca0ceaaf1a0e2
Martin Kuba <makub@ics.muni.cz>
2015-10-12 15:43:39 +0200

    fixed zip options to not ask for overwriting files

--- install_jslibs.sh
+++ install_jslibs.sh
@@ -4,9 +4,9 @@
 mkdir -p /data/wwwlibs
 cd /data/wwwlibs
 wget http://cdn.sencha.com/ext/gpl/ext-3.4.1.1-gpl.zip
-unzip -q ext-3.4.1.1-gpl.zip
+unzip -o -q ext-3.4.1.1-gpl.zip
 wget http://cdn.sencha.com/ext/gpl/ext-4.2.1-gpl.zip
-unzip -q ext-4.2.1-gpl.zip
+unzip -o -q ext-4.2.1-gpl.zip
 
 svn co svn://bnhelp.cz/hslayers/branches/hslayers-3.5
 cd hslayers-3.5/tools
