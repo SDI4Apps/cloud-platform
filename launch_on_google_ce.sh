0876c2d324f4bb28d34ae17a7f3795327b8ef87e
Martin Kuba <makub@ics.muni.cz>
2015-10-13 16:48:19 +0200

    updated to newer Ubuntu image

--- launch_on_google_ce.sh
+++ launch_on_google_ce.sh
@@ -2,5 +2,5 @@
 
 gcloud compute instances create mysdi4apps \
   --image-project ubuntu-os-cloud \
-  --image ubuntu-1404-trusty-v20150316 \
+  --image ubuntu-1404-trusty-v20150909a \
   --metadata-from-file user-data=user-data.yaml
