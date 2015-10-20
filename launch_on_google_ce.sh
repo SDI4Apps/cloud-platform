81da6b35d7eda20f5e004ee0e01206532b83cefd
Martin Kuba <makub@ics.muni.cz>
2015-10-20 09:14:18 +0200

    added unique instance names

--- launch_on_google_ce.sh
+++ launch_on_google_ce.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-gcloud compute instances create mysdi4apps \
+gcloud compute instances create sdi4apps-$(date '+%Y%m%d%H%M%S') --description "SDI4Apps platform 1.0" \
   --image-project ubuntu-os-cloud \
   --image ubuntu-1404-trusty-v20150909a \
+  --machine-type n1-standard-1 \
+  --zone europe-west1-c \
   --metadata-from-file user-data=user-data.yaml
